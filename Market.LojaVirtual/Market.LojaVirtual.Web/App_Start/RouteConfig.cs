99a44ed24e9e9826b0c48fca5a7d99d7e1ac7485
matheus.rodrigues <matheus.rodrigues@PC-ESTAG1>
2018-01-05 09:40:17 -0200

    Agora ta funcionando tudo que tem disponivel

--- Market.LojaVirtual/Market.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Market.LojaVirtual/Market.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Market.LojaVirtual.Web
@@ -13,12 +9,59 @@ namespace Market.LojaVirtual.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //1
+            routes.MapRoute(null,
+                "",
+                new{
+                    controller = "Vitrine"
+                    , action = "ListaProdutos"
+                    , categoria = (string) null
+                    , pagina = 1});
+            //2
+            routes.MapRoute(
+                null,
+                "Pagina{pagina}",
+                new { controller = "Vitrine",
+                    action = "ListaProdutos",
+                    categoria = (string)null
+                },
+                new { pagina = @"\d+" });
+            //3
+            routes.MapRoute(
+                null,
+                "{categoria}",
+                new
+                {
+                    controller = "Vitrine",
+                    action = "ListaProdutos",
+                    pagina = 1});
+
+            //4
+            routes.MapRoute(
+                null,
+                "{categoria}Pagina{pagina}",
+                new
+                {
+                    Controller = "Vitrine",
+                    action = "ListaProdutos"
+                },
+                new { pagina = @"\d+"});
+
             routes.MapRoute(
                 name: null,
                 url: "Pagina{pagina}",
                 defaults:new { controller = "Vitrine", action ="ListaProdutos" }
                 );
 
+            routes.MapRoute(
+                null,
+                "Pagina{pagina}",
+                new {Controller ="Vitrine",
+                    Action ="ListaProdutos",
+                    categoria =(string)null}, 
+                new {
+            pagina = @"\d+"
+        });
 
             routes.MapRoute(
                 name: "Default",
--- Market.LojaVirtual/Market.LojaVirtual.Web/Controllers/VitrineController.cs
+++ Market.LojaVirtual/Market.LojaVirtual.Web/Controllers/VitrineController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Market.LojaVirtual.Dominio.Repositorio;
 using Market.LojaVirtual.Web.Models;
 using Market.LojaVirtual.Web;
+using Market.LojaVirtual.Dominio.Entidades;
 
 namespace Market.LojaVirtual.Web.Controllers
 {
@@ -14,25 +15,38 @@ namespace Market.LojaVirtual.Web.Controllers
         private ProdutosRepositorio _repositorio;
         public int ProdutosPorPagina = 3;
 
-        public ViewResult ListaProdutos(int pagina = 1)
+        public ViewResult ListaProdutos(string categoria, int pagina = 1)
         {
             _repositorio = new ProdutosRepositorio();
 
+            IEnumerable<Produto> produtos = null;
+
+            if (categoria == null)
+                produtos = _repositorio.Produtos
+                    .OrderBy(p => p.Descricao)
+                    .Skip((pagina - 1) * ProdutosPorPagina)
+                    .Take(ProdutosPorPagina);
+            else
+                produtos = _repositorio.Produtos
+                        .Where(p => p.Categoria == categoria)
+                    .OrderBy(p => p.Descricao)
+                    .Skip((pagina - 1) * ProdutosPorPagina)
+                    .Take(ProdutosPorPagina);
+
             ProdutosViewModel model = new ProdutosViewModel
             {
-                Produtos = _repositorio.Produtos
-                .OrderBy(p => p.Descricao)
-                .Skip((pagina - 1) * ProdutosPorPagina)
-                .Take(ProdutosPorPagina),
-
+                Produtos = produtos,
                 Paginacao = new Paginacao
                 {
                     PaginaAtual = pagina,
                     ItensPorPagina = ProdutosPorPagina,
                     ItensTotal = _repositorio.Produtos.Count()
-                }
+                },
+
+                CategoriaAtual = categoria
             };
-            return View(model);
+
+            return View("~/Views/Vitrine/ListaProdutos.cshtml", model);
         }
     }
 }
